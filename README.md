86014dccfc62c3e8721eb32518cc935687058d19
VictorSilva12 <vgess@hotmail.com>
2020-08-20 14:43:42 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# csharp-webdriver
+Pratica de automação de testes utilizando C#, NUnit e Selenium
---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace NUnitTest
+{
+    public class Tests
+    {
+       
+        
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
